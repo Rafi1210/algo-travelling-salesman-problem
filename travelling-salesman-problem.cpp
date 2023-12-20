a33a032efef760f4a03b97f07afa9a5ec9fdfdef
Rafi1210 <sadatahmedrafi@gmail..com>
2023-12-21 02:50:16 +0600

    print matrix added

--- travelling-salesman-problem.cpp
+++ travelling-salesman-problem.cpp
@@ -1,11 +1,59 @@
-#include<bits/stdc++.h>
+#include <iostream>
+#include <vector>
 using namespace std;
 
-int salesman(int x[], int n){
+struct node {
+    int vertex, weight;
+};
 
+vector<struct node> ad[100] = {};
+
+void G(int u, int v, int w) {
+    struct node temp;
+    cout << "Enter weight between " << u << " and " << v << " paths : ";
+    cin >> w;
+
+        temp.vertex = v;
+        temp.weight = w;
+        ad[u].push_back(temp);
+
+        temp.vertex = u;  // Reverse the direction for an undirected graph
+        ad[v].push_back(temp);
 }
 
-int main(){
+void printMatrix(int n){
+    vector<vector<int>> matrix(n, vector<int>(n, 0)); //Initialize matrix with zero
+    for (int i = 1; i <= n; i++) {
+        for (int j = 0; j < ad[i].size(); j++) {
+            int v = ad[i][j].vertex;
+            int weight = ad[i][j].weight;
+            matrix[i - 1][v - 1] = weight;
+            matrix[v - 1][i - 1] = weight;  //An undirected graph so same weight in both index
+        }
+    }
+    // Print the matrix
+    cout << "\nAdjacency Matrix Path Weight Table:\n";
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cout << matrix[i][j] << "\t\t";
+        }
+        cout << endl;
+    }
+}
+
+int main() {
+    int n, e, u, v, w;
+
+    cout << "Enter number cities and paths : ";
+    cin >> n >> e;
+
+    for (int i = 1; i <= e; i++) {
+        cout << "Enter cities between path : "  ;
+        cin >> u >> v;
+        G(u, v, w);
+    }
+    printMatrix(n);
+
 
     return 0;
 }
